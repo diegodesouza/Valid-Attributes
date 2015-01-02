351625f1a524f303415a1b91ca9c5d56aa7a2366
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-01 22:32:06 -0500

    valid_attribute changes in test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 describe User do
-  it { should have_valid(:email).when('user@example.com') }
-  it { should_not have_valid(:email).when('user@mycom',
-    'usermy.com',
-    'user',
-    '.com',
-    'my.com') }
+  it { should have_valid(:first_name).when('John', 'Sally') }
+  it { should_not have_valid(:first_name).when(nil, '') }
+
+  it { should have_valid(:last_name).when('Smith', "O'Leary") }
+  it { should_not have_valid(:last_name).when(nil, '') }
 end
