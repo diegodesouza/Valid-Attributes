4d87c2e0b51824a059b337d0863b13e42d48ad86
Diego De Souza <a.t.dieps17@yahoo.com>
2015-01-01 22:32:52 -0500

    valid_attribute/blank_values method in test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe User do
+  let(:blank_values) { [nil, ''] }
   it { should have_valid(:first_name).when('John', 'Sally') }
-  it { should_not have_valid(:first_name).when(nil, '') }
+  it { should_not have_valid(:first_name).when(*blank_values) }
 
   it { should have_valid(:last_name).when('Smith', "O'Leary") }
-  it { should_not have_valid(:last_name).when(nil, '') }
+  it { should_not have_valid(:last_name).when(*blank_values) }
 end
